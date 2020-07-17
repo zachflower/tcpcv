d6aa6391d90b0f20bfcb198c9f3d8c2f9ea2b3ba
Zachary Flower <zach@zacharyflower.com>
2020-07-16 23:21:03 -0600

    spaces ftw

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ const sprintf = require("sprintf-js").sprintf;
 const vsprintf = require("sprintf-js").vsprintf;
 const wrap = require('word-wrap');
 
-
+const MOTD = process.env.MOTD || "Zachary Flower";
 const PORT = process.env.PORT || 2468;
+const RESUME = process.env.RESUME || "resume.json";
 
 try {
   require.resolve('./config');
@@ -21,7 +22,8 @@ try {
 }
 
 const config = require('./config');
-const stats = fs.statSync("resume.json");
+const stats = fs.statSync(RESUME);
+const resume = JSON.parse(fs.readFileSync(RESUME, 'utf8'));
 
 /*
  * Global Variables
@@ -33,175 +35,175 @@ let lastInput = '';
  * Cleans the input of carriage return, newline
  */
 const cleanInput = (data) => {
-	const ctrld = Buffer.from([04]);
+  const ctrld = Buffer.from([04]);
 
   /*
    * Convert Ctrl+D to 'exit'
    */
   if ( data.equals(ctrld) ) {
       return 'exit';
-	}
+  }
 
-	return data.toString().replace(/(\r\n|\n|\r)/gm,"").toLowerCase();
+  return data.toString().replace(/(\r\n|\n|\r)/gm,"").toLowerCase();
 };
 
 /*
  * Send Data to Socket
  */
 const sendData = (socket, data) => {
-	socket.write(data);
-	socket.write("$ ");
+  socket.write(data);
+  socket.write("$ ");
 };
 
 /*
  * Method executed when data is received from a socket
  */
 const receiveData = (socket, data) => {
-	let cleanData = cleanInput(data);
+  let cleanData = cleanInput(data);
 
-	if ( cleanData != '!!' ) {
-		lastInput = cleanData;
-	} else {
-		cleanData = lastInput;
-	}
+  if ( cleanData != '!!' ) {
+    lastInput = cleanData;
+  } else {
+    cleanData = lastInput;
+  }
 
-	let output = "";
+  let output = "";
 
-	switch ( cleanData ) {
+  switch ( cleanData ) {
     case '':
-			sendData(socket, output);
+      sendData(socket, output);
       break;
-		case 'quit':
-		case 'exit':
-			socket.end('Goodbye!\n');
-			break;
-		case 'help':
-			output += "These shell commands are defined internally.  Type 'help' to see this list.\n";
-			output += "Type 'help <command>' for more information about a particular command.\n";
+    case 'quit':
+    case 'exit':
+      socket.end('Goodbye!\n');
+      break;
+    case 'help':
+      output += "These shell commands are defined internally.  Type 'help' to see this list.\n";
+      output += "Type 'help <command>' for more information about a particular command.\n";
 
-			output += "\n";
-			output += "Commands:\n";
-			output += "  cv                         :  Display resume information\n";
-			output += "  exit                       :  Exit the resume\n";
-			output += "  help                       :  Display this help text\n";
-			output += "  quit                       :  Exit the resume\n";
-			output += "  resume                     :  Display resume information\n\n";
+      output += "\n";
+      output += "Commands:\n";
+      output += "  cv                         :  Display resume information\n";
+      output += "  exit                       :  Exit the resume\n";
+      output += "  help                       :  Display this help text\n";
+      output += "  quit                       :  Exit the resume\n";
+      output += "  resume                     :  Display resume information\n\n";
 
-			sendData(socket, output);
-			break;
-		case 'help cv':
-		case 'help resume':
-			output += "These shell commands are defined via a config file.  Type 'help resume' to see this list.\n";
-			output += "\n";
-			output += "Commands:\n";
-			output += "  resume                            :  Full resume\n";
+      sendData(socket, output);
+      break;
+    case 'help cv':
+    case 'help resume':
+      output += "These shell commands are defined via a config file.  Type 'help resume' to see this list.\n";
+      output += "\n";
+      output += "Commands:\n";
+      output += "  resume                            :  Full resume\n";
 
-			for ( section in config.sections ) {
-				output += vsprintf("  resume %-25s  :  %-25s\n", [section, config.sections[section].description]);
-			}
+      for ( section in config.sections ) {
+        output += vsprintf("  resume %-25s  :  %-25s\n", [section, config.sections[section].description]);
+      }
 
-			output += "\n";
+      output += "\n";
 
-			sendData(socket, output);
-			break;
-		case 'cv':
-		case 'resume':
-			for ( section in config.sections ) {
-				output += resumeSection(section);
-			}
+      sendData(socket, output);
+      break;
+    case 'cv':
+    case 'resume':
+      for ( section in config.sections ) {
+        output += resumeSection(section);
+      }
 
-			sendData(socket, output);
-			break;
-		default:
-			if ( cleanData.match(/^(resume|cv) /) ) {
-				const section = cleanData.replace(/^(resume|cv) /, '');
+      sendData(socket, output);
+      break;
+    default:
+      if ( cleanData.match(/^(resume|cv) /) ) {
+        const section = cleanData.replace(/^(resume|cv) /, '');
 
-				if ( config.sections.hasOwnProperty(section) ) {
-					output = resumeSection(section);
+        if ( config.sections.hasOwnProperty(section) ) {
+          output = resumeSection(section);
 
-					sendData(socket, output);
-					break;
-				}
-			}
+          sendData(socket, output);
+          break;
+        }
+      }
 
       sendData(socket, "-resume: " + cleanData + ": command not found\n");
-			break;
-	}
+      break;
+  }
 };
 
 const resumeSection = (section) => {
-	let output = "";
+  let output = "";
 
-	if ( config.sections.hasOwnProperty(section) ) {
-		output += "--------------------------------------------------------------------------------\n";
-		output += sprintf("%s\n", config.sections[section].title);
-		output += "--------------------------------------------------------------------------------\n";
+  if ( config.sections.hasOwnProperty(section) ) {
+    output += "--------------------------------------------------------------------------------\n";
+    output += sprintf("%s\n", config.sections[section].title);
+    output += "--------------------------------------------------------------------------------\n";
 
-		stringlast = false;
+    stringlast = false;
 
-		config.sections[section].data.forEach((block) => {
-			if ( typeof block === 'string' || block instanceof String ) {
-				output += sprintf("%s\n", block);
+    config.sections[section].data.forEach((block) => {
+      if ( typeof block === 'string' || block instanceof String ) {
+        output += sprintf("%s\n", block);
 
-				stringlast = true;
-			} else {
-				if ( block.hasOwnProperty('header') ) {
-					output += vsprintf("%-51s  :  %-24s\n", block.header);
-				}
+        stringlast = true;
+      } else {
+        if ( block.hasOwnProperty('header') ) {
+          output += vsprintf("%-51s  :  %-24s\n", block.header);
+        }
 
-				if ( block.hasOwnProperty('subheader') ) {
-					output += vsprintf("%-51s  :  %-24s\n", block.subheader);
-				}
+        if ( block.hasOwnProperty('subheader') ) {
+          output += vsprintf("%-51s  :  %-24s\n", block.subheader);
+        }
 
-				if ( block.hasOwnProperty('body') ) {
-					output += sprintf("%s\n", wrap(block.body, {indent: '    ', width: 76}));
-				}
+        if ( block.hasOwnProperty('body') ) {
+          output += sprintf("%s\n", wrap(block.body, {indent: '    ', width: 76}));
+        }
 
-				output += "\n";
+        output += "\n";
 
-				stringlast = false
-			}
-		});
+        stringlast = false
+      }
+    });
 
-		if ( stringlast ) {
-			output += "\n";
-		}
-	}
+    if ( stringlast ) {
+      output += "\n";
+    }
+  }
 
-	return output;
+  return output;
 };
 
 /*
  * Method executed when a socket ends
  */
 const closeSocket = (socket) => {
-	const i = sockets.indexOf(socket);
+  const i = sockets.indexOf(socket);
 
-	if (i != -1) {
-		sockets.splice(i, 1);
-	}
+  if (i != -1) {
+    sockets.splice(i, 1);
+  }
 };
 
 /*
  * Callback method executed when a new TCP socket is opened.
  */
 const newSocket = (socket) => {
-	sockets.push(socket);
-	socket.write("\n");
-	socket.write("Last updated: " + stats.mtime.toUTCString() + " by Zachary Flower\n");
-	socket.write("\n");
-	socket.write(figlet.textSync(config.motd));
-	socket.write("\n");
+  sockets.push(socket);
+  socket.write("\n");
+  socket.write("Last updated: " + stats.mtime.toUTCString() + " by Zachary Flower\n");
+  socket.write("\n");
+  socket.write(figlet.textSync(MOTD));
+  socket.write("\n");
 
-	sendData(socket, "Type 'help' for more information.\n");
+  sendData(socket, "Type 'help' for more information.\n");
 
-	socket.on('data', (data) => {
-		receiveData(socket, data);
-	});
+  socket.on('data', (data) => {
+    receiveData(socket, data);
+  });
 
-	socket.on('end', () => {
-		closeSocket(socket);
-	});
+  socket.on('end', () => {
+    closeSocket(socket);
+  });
 };
 
 const server = net.createServer(newSocket);
